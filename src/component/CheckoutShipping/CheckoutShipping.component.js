def336488a076ccf111dba54db9dfdca93661751
shaimaayosef <shimaa.sayed8888@gmail.com>
2022-09-06 16:59:29 +0200

    override

---
+++ src/component/CheckoutShipping/CheckoutShipping.component.js
@@ -0,0 +1,9 @@
+import {
+    CheckoutShipping as SourceCheckoutShipping,
+} from 'SourceComponent/CheckoutShipping/CheckoutShipping.component';
+
+export class CheckoutShipping extends SourceCheckoutShipping {
+    // TODO implement logic
+};
+
+export default CheckoutShipping;
